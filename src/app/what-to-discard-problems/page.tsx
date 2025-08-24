9f0ed18993184fcdc829de0271f82da34a4e603f
okki-0417 <ochan0901@gmail.com>
2025-08-24 20:52:32 +0900

    Revert "chore: 何切る問題をPPRにした"
    This reverts commit 0ba63cedacaea172f975fcc9ffac46373e892838.

--- src/app/what-to-discard-problems/page.tsx
+++ src/app/what-to-discard-problems/page.tsx
@@ -19,8 +19,6 @@ export const metadata: Metadata = {
   },
 };
 
-export const experimental_ppr = true;
-
 export default function WhatToDiscardProblems() {
   return (
     <Container mt="20" maxW="4xl" px={["3", "0"]} mb="20">
