9a0ce36aa4b32b4fcbaa4b783abfda7a3bf58859
okki-0417 <ochan0901@gmail.com>
2025-09-28 10:38:27 +0900

    test: prettier auto-fix

---
+++ test-prettier.tsx
@@ -0,0 +1,13 @@
+// Test file for prettier auto-fix
+import React from "react";
+
+const TestComponent = () => {
+  return (
+    <div>
+      <p>This should be formatted</p>
+      <span>With multiple spaces</span>
+    </div>
+  );
+};
+
+export default TestComponent;
