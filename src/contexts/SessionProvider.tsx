1537adc20b9cdd3711862cc8258d0dcf491ca576
okki-0417 <ochan0901@gmail.com>
2025-11-02 16:45:47 +0900

    refactor: SessionProviderのsessionの取り方をuseQueryとuseEffectでrefactor

--- src/contexts/SessionProvider.tsx
+++ src/contexts/SessionProvider.tsx
@@ -1,22 +1,27 @@
 "use client";
 
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { useQuery } from "@apollo/client/react";
 import { CurrentSessionDocument, CurrentSessionQuery, Session } from "@/src/generated/graphql";
-import { ErrorLike } from "@apollo/client";
 
 type SessionContextType = {
   session: CurrentSessionQuery["currentSession"] | null;
-  loading: boolean;
-  error: ErrorLike;
   updateSession: () => Promise<void>;
 };
 
 export const SessionContext = createContext<SessionContextType | null>(null);
 
 export function SessionProvider({ children }: { children: ReactNode }) {
-  const { data, loading, error, refetch } = useQuery(CurrentSessionDocument);
-  const [session, setSession] = useState<Session | null>(data?.currentSession || null);
+  const { data, refetch } = useQuery(CurrentSessionDocument);
+  const [session, setSession] = useState<Session | null>(null);
+
+  // useQueryを呼んだ直後はdataがundefinedなので、dataが更新されたときにsessionをセットする
+  // その他にdataが変わるケースは無く、初回のみロードされる想定
+  useEffect(() => {
+    if (!data?.currentSession) return;
+
+    setSession(data.currentSession);
+  }, [data]);
 
   const updateSession = async () => {
     const refetchData = await refetch();
@@ -24,8 +29,6 @@ export function SessionProvider({ children }: { children: ReactNode }) {
   };
 
   return (
-    <SessionContext.Provider value={{ session, loading, error, updateSession }}>
-      {children}
-    </SessionContext.Provider>
+    <SessionContext.Provider value={{ session, updateSession }}>{children}</SessionContext.Provider>
   );
 }
